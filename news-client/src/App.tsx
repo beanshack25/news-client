10f112fe17011fd06fb53816d94632e8a5128f18
Theo Akande <ta722@ic.ac.uk>
2025-02-02 09:16:35 +0000

    fix: plugin api routes

--- news-client/src/App.tsx
+++ news-client/src/App.tsx
@@ -73,11 +73,7 @@ function App() {
   const [currentScroll, setCurrentScroll] = useState(0);
 
   useEffect(() => {
-    const url =
-      "https://edition.cnn.com/2025/02/01/politics/mexico-canada-china-tariffs-trump/index.html";
-    const endpoint = `http://localhost:5000/api/start?${new URLSearchParams({
-      url,
-    })}`;
+    const endpoint = `http://localhost:5000/api/start`;
 
     fetch(endpoint, {
       method: "GET",
