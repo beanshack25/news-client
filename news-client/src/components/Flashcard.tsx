377abdb1daf1fed781343f9aca4824e8f9982746
jamacz <jack.mccomish@icloud.com>
2025-02-02 10:08:44 +0000

    make cards white

--- news-client/src/components/Flashcard.tsx
+++ news-client/src/components/Flashcard.tsx
@@ -14,16 +14,19 @@ export default function Flashcard({
   scrollRight: (url: string) => void;
 }) {
   return (
-    <div className="flex flex-col bg-slate-100 rounded-xl p-8 w-card h-card shadow-lg">
+    <div className="flex flex-col bg-white rounded-xl p-8 w-card h-card shadow-lg">
       <h1 className="text-2xl font-semibold mb-5">{card.title}</h1>
       <div className="relative flex-1 overflow-y-hidden">
         {card.content.split("\n").map((i) => (
           <p className="mb-2">{i}</p>
         ))}
-        <div className="absolute bottom-0 w-full h-24 bg-gradient-to-b from-transparent to-slate-100"></div>
+        <div className="absolute bottom-0 w-full h-24 bg-gradient-to-b from-transparent to-white"></div>
       </div>
       <div className="flex justify-between mt-2">
-        <a href={card.link} className="cursor-pointer bg-slate-400 rounded-lg py-1.5 px-6 text-white font-semibold">
+        <a
+          href={card.link}
+          className="cursor-pointer bg-slate-400 rounded-lg py-1.5 px-6 text-white font-semibold"
+        >
           View Article
         </a>
         <button
